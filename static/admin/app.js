b458295f4ad03f8a9c2478286b35ebe5d8263b35
caixw <xnotepad@gmail.com>
2015-10-03 10:59:49 +0800

    将部分公用函数放到App命名空间下，减少命名污染

--- static/admin/app.js
+++ static/admin/app.js
@@ -4,189 +4,168 @@
 // @copyright 2015 by caixw
 // @link https://github.com/caixw/typing
 
-// BUG(caixw): 每个页面必须得有一个pageInit函数，否则会调用上一个页面的pageInit函数。
+function App(options){
+    var defaults = {
+        // api的统一前缀
+        adminAPIPrefix: '/admin/api',
+        frontAPIPrefix: '/api',
+        themeURLPrefix: '/themes',
 
-// api的统一前缀
-var adminAPIPrefix = '/admin/api';
-var frontAPIPrefix = '/api';
-var themeURLPrefix = '/themes';
+        // 每页加载的记录数量
+        size: 25,
 
-// 每页加载的记录数量
-var size = 25;
+        // 标题设置项
+        titleSuffix:    'typing',
+        titleSeparator: '-',
 
-// 标题设置项
-var titleSuffix = 'typing';
-var titleSeparator = '-';
+        // 提示信息关闭时间
+        messageTimeout: 5000
+    };
+    var opt = $.extend({}, defaults, options);
 
-// 提示信息关闭时间
-var messageTimeout = 5000;
+    // 设置标题
+    this.setTitle = function(title) {
+        if (!title){
+            title = opt.titleSuffix
+        }else{
+            title = title + opt.titleSeparator + opt.titleSuffix;
+        }
+        $('html head title').html(title);
+    };
 
-// 将tpl模板的内容解析并插入到container中，data为传递给模板的数据。
-function loadTemplate(containerSelector, templateSelector, data) {
-    var source = $(templateSelector).html();
-    var tpl = Handlebars.compile(source);
-    $(containerSelector).append(tpl(data));
-}
+    this.delete = function(settings) {
+        settings.contentType = 'application/json;charset=utf-8';
+        settings.dataType    = 'json';
+        settings.method      = 'DELETE';
+        settings.headers     = {'Authorization': window.sessionStorage.token};
+        return $.ajax(settings);
+    };
 
-// 加载指定的模板页面，该页页会自动包含菜单等内容。
-// 若未登录，是会自动跳转到登录页面。
-// 除了模板名称之外，还可参传递其它任何参数给loadPage，一般为路由匹配项上的参数。
-// 若存在这些参数，则会尝试调用加载页面的pageInit函数来做一些初始化。
-function loadBodyPage(template) {
-    if (!isLogin()){
-        redirect('login')
-        return
-    }
+    this.post = function(settings) {
+        settings.contentType = 'application/json;charset=utf-8';
+        settings.dataType    = 'json';
+        settings.method      = 'POST';
+        settings.headers     = {'Authorization': window.sessionStorage.token};
+        return $.ajax(settings);
+    };
 
-    var args = [];
-    Array.prototype.push.apply(args, arguments);
-    args.shift();
+    this.put = function(settings) {
+        settings.contentType = 'application/json;charset=utf-8';
+        settings.dataType    = 'json';
+        settings.method      = 'PUT';
+        settings.headers     = {'Authorization': window.sessionStorage.token};
+        return $.ajax(settings);
+    };
 
-    $('body').load('./body.html', function(){
-        $('#content').load(template, function(){
+    this.get = function(settings) {
+        settings.contentType = 'application/json;charset=utf-8';
+        settings.dataType    = 'json';
+        settings.method      = 'GET';
+        settings.headers     = {'Authorization': window.sessionStorage.token};
+        return $.ajax(settings);
+    };
+
+    this.patch = function(settings) {
+        settings.contentType = 'application/json;charset=utf-8';
+        settings.dataType    = 'json';
+        settings.method      = 'PATCH';
+        settings.headers     = {'Authorization': window.sessionStorage.token};
+        return $.ajax(settings);
+    };
+
+    // 向elem元素输出color颜色的信息
+    this.showMessage = function(color, message) {
+        var div = $('<div id="message" class="ui '+color+' visible message">'+message+'</div>');
+        div.hide();
+        $('.message-row').prepend(div);
+        div.slideDown();
+
+        window.setTimeout(function() {
+            div.slideUp(function(){div.remove();});
+        }, opt.messageTimeout);
+    };
+
+    // 将tpl模板的内容解析并插入到container中，data为传递给模板的数据。
+    this.loadTemplate = function(containerSelector, templateSelector, data) {
+        var source = $(templateSelector).html();
+        var tpl = Handlebars.compile(source);
+        $(containerSelector).append(tpl(data));
+    };
+
+    // 加载指定的模板页面，该页页会自动包含菜单等内容。
+    // 若未登录，是会自动跳转到登录页面。
+    // 除了模板名称之外，还可参传递其它任何参数给loadPage，一般为路由匹配项上的参数。
+    // 若存在这些参数，则会尝试调用加载页面的pageInit函数来做一些初始化。
+    this.loadBodyPage = function(template) {
+        if (!this.isLogin()){
+            this.redirect('login')
+            return
+        }
+
+        var args = [];
+        Array.prototype.push.apply(args, arguments);
+        args.shift();
+
+        $('body').load('./body.html', function(){
+            $('#content').load(template, function(){
+                if (typeof(pageInit) == 'function'){
+                    pageInit.apply(null, args);
+                }
+            });
+        });
+    };
+
+    this.loadPage = function(template){
+        var args = [];
+        Array.prototype.push.apply(args, arguments);
+        args.shift();
+
+        $('body').load(template, function(){
             if (typeof(pageInit) == 'function'){
                 pageInit.apply(null, args);
             }
         });
-    });
-}
-
-function loadPage(template){
-    var args = [];
-    Array.prototype.push.apply(args, arguments);
-    args.shift();
-
-    $('body').load(template, function(){
-        if (typeof(pageInit) == 'function'){
-            pageInit.apply(null, args);
-        }
-    });
-}
-
-// 加载登录页面，若已经登录，则会跳转到dashboard页面。
-function loadLoginPage() {
-    if (isLogin()){
-        redirect('dashboard');
-        return
-    }
-
-    loadPage('login.html');
-}
-
-
-// 判断是否已经登录。
-function isLogin() {
-    return !!window.sessionStorage.token;
-}
-
-// 跳转到指定页页。
-function redirect(fregment) {
-    if(fregment.charAt(0) != '#'){
-        fregment = '#' + fregment;
-    }
-    window.location.href = fregment;
-}
-
-// 设置当前页面的标题
-function setTitle(title) {
-    if (!title){
-        title = titleSuffix
-    }else{
-        title = title + titleSeparator + titleSuffix;
-    }
-    $('html head title').html(title);
-}
-
-// form中所有包含name的任意元素将被构建成一个JSON
-function buildJSON(form) {
-    var ret = {};
-    form.find('*[name]').each(function(index, elem){
-        var v = $(elem).val();
-        // TODO 考虑其它类型的情况
-        if ($(elem).attr('type') == 'number'){
-            v = parseInt(v);
-        }
-        ret[$(elem).attr('name')] = v;
-    });
-    return JSON.stringify(ret);
-}
-
-function del(settings) {
-    settings.method  = 'DELETE';
-    settings.headers = {'Authorization': window.sessionStorage.token};
-    return $.ajax(settings);
-}
-
-function post(settings) {
-    settings.method  = 'POST';
-    settings.headers = {'Authorization': window.sessionStorage.token};
-    return $.ajax(settings);
-}
-
-function put(settings) {
-    settings.method  = 'PUT';
-    settings.headers = {'Authorization': window.sessionStorage.token};
-    return $.ajax(settings);
-}
-
-function get(settings) {
-    settings.method  = 'GET';
-    settings.headers = {'Authorization': window.sessionStorage.token};
-    return $.ajax(settings);
-}
-
-function patch(settings) {
-    settings.method  = 'PATCH';
-    settings.headers = {'Authorization': window.sessionStorage.token};
-    return $.ajax(settings);
-}
-
-// 向elem元素输出color颜色的信息
-function showMessage(color, message) {
-    var div = $('<div id="message" class="ui '+color+' visible message">'+message+'</div>');
-    div.hide();
-    $('.message-row').prepend(div);
-    div.slideDown();
-
-    window.setTimeout(function() {
-        div.slideUp(function(){div.remove();});
-    }, messageTimeout);
-}
-
-$(document).ready(function() {
-    $.ajaxSetup({
-        'contentType': 'application/json;charset=utf-8', // 请求提交类型
-        'dataType':    'json',                           // 服务器返回类型
-    });
-
-    var routes = {
-        '':
-                function(){ loadLoginPage(); },
-        'login':
-                function(){ loadLoginPage(); },
-        'logout':
-                function(){ loadPage('logout.html'); },
-        'dashboard':
-                function(){ loadBodyPage('dashboard.html'); },
-        'settings/system':
-                function(){ loadBodyPage('settings-system.html'); },
-        'settings/users':
-                function(){ loadBodyPage('settings-users.html'); },
-        'settings/themes':
-                function(){ loadBodyPage('settings-themes.html'); },
-        'settings/sitemap':
-                function(){ loadBodyPage('settings-sitemap.html'); },
-        'metas/tags':
-                function(){ loadBodyPage('metas-tags.html'); },
-        'metas/cats':
-                function(){ loadBodyPage('metas-cats.html'); },
-        'posts/list':
-                function(){ loadBodyPage('posts-list.html'); },
-        'posts/edit/:id':
-                function(){ loadBodyPage('posts-edit.html'); },
-        'comments/list':
-                function(){ loadBodyPage('comments-list.html'); },
     };
 
-    var router = Router(routes).init();
-});
+    // 加载登录页面，若已经登录，则会跳转到dashboard页面。
+    this.loadLoginPage = function() {
+        if (this.isLogin()){
+            this.redirect('dashboard');
+            return
+        }
+
+        this.loadPage('login.html');
+    };
+
+
+    // 判断是否已经登录。
+    this.isLogin = function() {
+        return !!window.sessionStorage.token;
+    };
+
+    // 跳转到指定页页。
+    this.redirect = function(fregment) {
+        if(fregment.charAt(0) != '#'){
+            fregment = '#' + fregment;
+        }
+        window.location.href = fregment;
+    };
+
+    // form中所有包含name的任意元素将被构建成一个JSON
+    this.buildJSON = function(form) {
+        var ret = {};
+        form.find('*[name]').each(function(index, elem){
+            var v = $(elem).val();
+            // TODO 考虑其它类型的情况
+            if ($(elem).attr('type') == 'number'){
+                v = parseInt(v);
+            }
+            ret[$(elem).attr('name')] = v;
+        });
+        return JSON.stringify(ret);
+    };
+
+    this.listen = function(routes) {
+        var router = Router(routes).init();
+    };
+} // end App
