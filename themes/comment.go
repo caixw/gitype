259d3cccadb68abae4954e2163e8ec8260106c4e
caixw <xnotepad@gmail.com>
2015-10-25 23:00:24 +0800

    添加themes.Comment

--- themes/comment.go
+++ themes/comment.go
@@ -4,5 +4,28 @@
 
 package themes
 
+import (
+	"strconv"
+	"time"
+)
+
 type Comment struct {
+	post *Post
+
+	ID         int64
+	Created    int64
+	IP         string
+	Agent      string
+	Content    string
+	IsAdmin    bool
+	AuthorName string
+	AuthorURL  string
+}
+
+func (c *Comment) CreatedFormat() string {
+	return time.Unix(c.Created, 0).Format(opt.DateFormat)
+}
+
+func (c *Comment) Permalink() string {
+	return c.post.Permalink() + "#comments-" + strconv.FormatInt(c.ID, 10)
 }
