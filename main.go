e8f1ded8f9187c9ee997e1abe321b72970fb0767
caixw <xnotepad@gmail.com>
2016-01-23 20:14:28 +0800

    添加命令行参数的使用说明

--- main.go
+++ main.go
@@ -12,26 +12,53 @@ import (
 	"github.com/caixw/typing/app"
 	"github.com/caixw/typing/feed"
 	"github.com/caixw/typing/front"
+	"github.com/issue9/utils"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const usage = `typing 一个简单的博客程序，支持以下两个参数：
-appdir 指定程序的数据存放路径，未指定，则为./config/；
-install 若指定了值，则为相应的安装过程`
+const usage = `typing 一个简单博客程序。
+源代码以MIT开源许可，并发布于github: https://github.com/caixw/typing
+
+命令行语法：
+ typing [options]
+
+ options:
+  -help    显示帮助信息；
+  -appdir  指定程序的数据存放路径，未指定，则为./config/；
+  -install 若指定了值，则为执行相应的安装过程可选值为:
+      -config 在appdir/config/下输出配置文件；
+      -db     向数据库创建表及输出默认的数据项
+
+常见用法：
+ 运行程序：    typing -appdir=/path
+ 输出配置文件: typing -appdir=/path -install=config
+ 创建数据库：  typing -appdir=/path -install=db`
 
 func main() {
 	flag.Usage = func() { fmt.Println(usage) }
+	help := flag.Bool("help", false, "显示帮助信息")
 	appdir := flag.String("appdir", "./config/", "指定程序的数据存放目录")
 	action := flag.String("install", "", "指定需要初始化的内容，可取的值可以为：config和db。")
 	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	if len(*appdir) == 0 || utils.FileExists(*appdir) {
+		fmt.Printf("appdir参数[%v]指定的目录不存在\n", *appdir)
+		return
+	}
 
 	if len(*action) > 0 { // 执行安装过程
 		if err := app.Install(*appdir, *action); err != nil {
 			panic(err)
 		}
+		return
 	}
 
 	// app
