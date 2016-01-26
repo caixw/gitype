40383ef65a4851f3e3a7235a7ef4775ed66e36a6
caixw <xnotepad@gmail.com>
2016-01-26 19:23:26 +0800

    更新文档

--- admin/admin.go
+++ admin/admin.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
+// 后台管理模块的相关api
 package admin
 
 import (
--- front/front.go
+++ front/front.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
+// 前端的相关功能。
 package front
 
 import (
