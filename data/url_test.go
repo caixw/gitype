5e98550c6772507f798cd4d175fa9ed568c84f61
caixw <xnotepad@gmail.com>
2016-02-27 15:43:19 +0800

    添加测试内容

--- data/url_test.go
+++ data/url_test.go
@@ -3,3 +3,19 @@
 // license that can be found in the LICENSE file.
 
 package data
+
+import (
+	"testing"
+
+	"github.com/issue9/assert"
+)
+
+func TestLoadURLS(t *testing.T) {
+	a := assert.New(t)
+
+	d := &Data{}
+	a.NotError(d.loadURLS("./testdata/meta/urls.yaml"))
+	urls := d.URLS
+	a.NotNil(urls)
+	a.Equal(urls.Post, "/posts").Equal(urls.Atom, "/atom.xml")
+}
