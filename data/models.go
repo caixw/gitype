f060a65311bb47625efa2cd2990b97df8ef55b48
caixw <xnotepad@gmail.com>
2017-05-19 22:39:04 +0800

    将 urls 作为 config 的一个子元素

--- data/models.go
+++ data/models.go
@@ -37,7 +37,7 @@ type Post struct {
 	CreatedFormat  string  `yaml:"created"`  // 创建时间的字符串表示形式
 	ModifiedFormat string  `yaml:"modified"` // 修改时间的字符串表示形式
 	TagsString     string  `yaml:"tags"`     // 关联标签的列表
-	Path           string  `yaml:"path"`     // 正文的文件名，相对于meta.yaml所在的目录
+	Path           string  `yaml:"path"`     // 正文的文件名，相对于 meta.yaml 所在的目录
 	Permalink      string  `yaml:"-"`        // 文章的唯一链接
 }
 
@@ -49,35 +49,26 @@ type Link struct {
 	Text  string `yaml:"text"`            // 链接的文本
 }
 
-// URLS 自定义 URL
-type URLS struct {
-	Root   string `yaml:"root"`   // 根地址
-	Suffix string `yaml:"suffix"` // 地址后缀
-	Posts  string `yaml:"posts"`  // 列表页地址
-	Post   string `yaml:"post"`   // 文章详细页地址
-	Tags   string `yaml:"tags"`   // 标签列表页地址
-	Tag    string `yaml:"tag"`    // 标签详细页地址
-	Search string `yaml:"search"` // 搜索URL，会加上Suffix作为后缀
-	Themes string `yaml:"themes"` // 主题地址
-}
-
 // Config 一些基本配置项。
 type Config struct {
 	Title           string `yaml:"title"`                 // 网站标题
 	Subtitle        string `yaml:"subtitle,omitempty"`    // 网站副标题
 	URL             string `yaml:"url"`                   // 网站的地址
-	Keywords        string `yaml:"keywords,omitempty"`    // 默认情况下的keyword内容
-	Description     string `yaml:"description,omitempty"` // 默认情况下的descrription内容
+	Keywords        string `yaml:"keywords,omitempty"`    // 默认情况下的 keyword 内容
+	Description     string `yaml:"description,omitempty"` // 默认情况下的 descrription 内容
 	Beian           string `yaml:"beian,omitempty"`       // 备案号
-	Uptime          int64  `yaml:"-"`                     // 上线时间，unix时间戳，由UptimeFormat转换而来
+	Uptime          int64  `yaml:"-"`                     // 上线时间，unix 时间戳，由 UptimeFormat 转换而来
 	UptimeFormat    string `yaml:"uptime"`                // 上线时间，字符串表示
 	PageSize        int    `yaml:"pageSize"`              // 每页显示的数量
 	LongDateFormat  string `yaml:"longDateFormat"`        // 长时间的显示格式
 	ShortDateFormat string `yaml:"shortDateFormat"`       // 短时间的显示格式
 	Theme           string `yaml:"theme"`                 // 默认主题
 
-	Menus  []*Link `yaml:"menus,omitempty"` // 菜单内容
-	Author *Author `yaml:"author"`          // 默认的作者信息
+	Author *Author `yaml:"author"` // 默认的作者信息
+
+	Menus []*Link `yaml:"menus,omitempty"` // 菜单内容
+
+	URLS *URLS `yaml:"urls"` // 自定义的路径内容
 
 	// feeds
 	RSS     *RSS     `yaml:"rss,omitempty"`
@@ -100,4 +91,16 @@ type Sitemap struct {
 	PostPriority   float64 `yaml:"postPriority"`
 	TagChangefreq  string  `yaml:"tagChangefreq"`
 	PostChangefreq string  `yaml:"postChangefreq"`
+}
+
+// URLS 自定义 URL
+type URLS struct {
+	Root   string `yaml:"root"`   // 根地址
+	Suffix string `yaml:"suffix"` // 地址后缀
+	Posts  string `yaml:"posts"`  // 列表页地址
+	Post   string `yaml:"post"`   // 文章详细页地址
+	Tags   string `yaml:"tags"`   // 标签列表页地址
+	Tag    string `yaml:"tag"`    // 标签详细页地址
+	Search string `yaml:"search"` // 搜索URL，会加上 Suffix 作为后缀
+	Themes string `yaml:"themes"` // 主题地址
 }
