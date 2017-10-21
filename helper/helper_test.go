fa0c7360d82d2c6adfa4f9e1af1141b72b5d2f52
caixw <xnotepad@gmail.com>
2017-10-21 23:47:37 +0800

    添加测试内容

---
+++ helper/helper_test.go
@@ -0,0 +1,19 @@
+// Copyright 2017 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package helper
+
+import (
+	"testing"
+
+	"github.com/issue9/assert"
+)
+
+func TestReplaceContent(t *testing.T) {
+	a := assert.New(t)
+
+	a.Equal("abc", ReplaceContent("abc%content%", ""))
+	a.Equal("abcTITLE", ReplaceContent("abc%content%", "TITLE"))
+	a.Equal("abc%Content%", ReplaceContent("abc%Content%", "TITLE"))
+}
