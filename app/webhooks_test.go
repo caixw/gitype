0ba9b3cef47f6ff5786de4909dfb9caba97b865d
caixw <xnotepad@gmail.com>
2018-09-16 21:46:25 +0800

    添加测试内容

---
+++ app/webhooks_test.go
@@ -0,0 +1,9 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package app
+
+import "github.com/issue9/web/config"
+
+var _ config.Sanitizer = &webhook{}
